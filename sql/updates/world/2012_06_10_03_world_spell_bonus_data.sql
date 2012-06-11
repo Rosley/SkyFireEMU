a74e21e705a48771bcba739d97a434b8e7f3b895
Bootz <bootz@projectskyfire.org>
2012-06-11 01:35:33 -0500

    missed two sqls

---
+++ sql/updates/world/2012_06_10_03_world_spell_bonus_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO `spell_bonus_data` VALUES ('9007', '0', '0', '0', '0.03', 'Druid - Pounce Bleed');
+INSERT INTO `spell_bonus_data` VALUES ('64569', '0', '0', '0', '0', 'Item - Blood Reserve');
+INSERT INTO `spell_bonus_data` VALUES ('17712', '0', '0', '0', '0', 'Item - Lifestone Healing');
+INSERT INTO `spell_bonus_data` VALUES ('5707', '0', '0', '0', '0', 'Item - Lifestone Regeneration');
+INSERT INTO `spell_bonus_data` VALUES ('71824', '0', '0', '0', '0', 'Item - Shaman T9 Elemental 4P Bonus');
+INSERT INTO `spell_bonus_data` VALUES ('21179', '0', '0', '0', '0', 'Item - Six Demon Bag - Chain Lightning');
+INSERT INTO `spell_bonus_data` VALUES ('15662', '0', '0', '0', '0', 'Item - Six Demon Bag - Fireball');
+INSERT INTO `spell_bonus_data` VALUES ('11538', '0', '0', '0', '0', 'Item - Six Demon Bag - Frostbolt');
